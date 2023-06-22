2ae720cc3297183465eadd6c078c4897e5a67f8d
kongkham-lk <kongkham.luangkhot@gmail.com>
2023-06-22 14:52:36 -0700

    initial define ConvertRequest class

--- backend/src/main/java/io/kongkham/kcurr/ConvertRequest.java
+++ backend/src/main/java/io/kongkham/kcurr/ConvertRequest.java
@@ -4,8 +4,22 @@ import lombok.Data;
 
 @Data
 public class ConvertRequest {
-    public double amount;
-    public String sourceCurr;
-    public String targetCurr;
+    private double amount;
+    private String sourceCurr;
+    private String targetCurr;
 
+    public ConvertRequest(double amount, String sourceCurr, String targetCurr) {
+        this.amount = amount;
+        this.sourceCurr = sourceCurr;
+        this.targetCurr = targetCurr;
+    }
+    public double convert() {
+        return amount * checkRate(sourceCurr) / checkRate(targetCurr);
+    }
+
+    private double checkRate(String curr) {
+        // get rate from api
+        double rate;
+        return 0;
+    }
 }
--- backend/src/main/java/io/kongkham/kcurr/CurrController.java
+++ backend/src/main/java/io/kongkham/kcurr/CurrController.java
@@ -1,14 +1,11 @@
 package io.kongkham.kcurr;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CurrController {
     @PostMapping("/convert")
     public double convertCurr(@RequestBody ConvertRequest data) {
-        return 5;
+        return 5.5;
     }
 }
