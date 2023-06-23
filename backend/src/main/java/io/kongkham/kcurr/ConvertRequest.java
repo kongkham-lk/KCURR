152b2fc2108d67a270ad556551c5a7d568fae60e
kongkham-lk <kongkham.luangkhot@gmail.com>
2023-06-23 00:00:22 -0700

    all ConvertRequest class's setter and getter method and comment for validation code that need to be included

--- backend/src/main/java/io/kongkham/kcurr/ConvertRequest.java
+++ backend/src/main/java/io/kongkham/kcurr/ConvertRequest.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class ConvertRequest {
+    // validate amount to greater than 0
     private double amount;
+    // validate both currency type
     private String sourceCurr;
     private String targetCurr;
 
@@ -13,7 +15,26 @@ public class ConvertRequest {
         this.sourceCurr = sourceCurr;
         this.targetCurr = targetCurr;
     }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    public void setCurr(String sourceCurr, String targetCurr) {
+        this.sourceCurr = sourceCurr;
+        this.targetCurr = targetCurr;
+    }
+    public double getAmount() {
+        return this.amount;
+    }
+    public String getCurr() {
+        return this.sourceCurr + " to " + this.targetCurr;
+    }
+
     public double convert() {
+        return amount * checkRate(sourceCurr) / checkRate(targetCurr);
+    }
+
+    public double convert(double amount, String sourceCurr, String targetCurr) {
         return amount * checkRate(sourceCurr) / checkRate(targetCurr);
     }
 
@@ -21,5 +42,9 @@ public class ConvertRequest {
         // get rate from api
         double rate;
         return 0;
+    }
+
+    public String toString() {
+        return "Amount: " + amount + ", from: " + sourceCurr + ", to: " + targetCurr;
     }
 }
--- backend/src/main/java/io/kongkham/kcurr/CurrController.java
+++ backend/src/main/java/io/kongkham/kcurr/CurrController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CurrController {
-    @PostMapping("/convert")
-    public double convertCurr(@RequestBody ConvertRequest data) {
-        return 5.5;
+    @PostMapping("/convert") // how @RequestBody pass those 3 values and store to the ConvertRequest instantiate
+    public double convertCurr(@RequestBody ConvertRequest data) { // @RequestBody can serve only 1 argument (no duplicated use)
+        // validate if amount to is greater than 0
+        // validate if currency type is valid
+        // FOUND HOW TO VALIDATE BUT DON KNOW HOW TO ADD DEPENDENCY TO build.gradle
+        return data.convert();
     }
 }
