313a1bc12449015bd4f8e96eec924dee64b747aa
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-05-09 14:41:12 -0700

    flagAPI only accept lowerCase of country code

--- frontend/src/util/getFlag.js
+++ frontend/src/util/getFlag.js
@@ -7,7 +7,7 @@ export function getFlag(currCountry) {
 };
 
 const getBaseUrl = (currCountry) => {
-    return `https://flagcdn.com/32x24/${currCountry.substring(0, 2)}.png`;
+    return `https://flagcdn.com/32x24/${currCountry.substring(0, 2).toLowerCase()}.png`;
 }
 
 const style = {
