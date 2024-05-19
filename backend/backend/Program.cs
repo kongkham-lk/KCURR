fe3f8e2541ed7be524415fbea1df86ed2858ed04
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-05-18 23:50:18 -0700

    manually specify allowed CORS

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -10,16 +10,20 @@ var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 var devBaseURL = "http://localhost:3000";
-var prodBaseURL = "https://kcurr.onrender.com/";
+var prodBaseURL = "https://kcurr.onrender.com";
+
+var origins = new string[] { devBaseURL, prodBaseURL };
+var methods = new string[] { "GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS", "CONNECT", "TRACE" };
+var headers = new string[] { "Content-Type", "Authorization", "X-Content-Type-Options", "Accept", "X-Requested-With", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers" };
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy  =>
         {
-            policy.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
+            policy.WithOrigins(origins)
+                .WithMethods(methods)
+                .WithHeaders(headers);
         });
 });
 builder.Services.AddControllers();
