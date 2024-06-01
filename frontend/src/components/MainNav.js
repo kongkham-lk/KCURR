5f65ae3462fd86e969d2ad3332c7e057ff000cb2
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-06-01 16:50:08 -0700

    Update MainNav.js
    - let top bar stay the top layer

--- frontend/src/components/MainNav.js
+++ frontend/src/components/MainNav.js
@@ -24,7 +24,7 @@ export default function MainNav(props) {
 
   return (
     <Box display='flex' sx={isDisplaySM ? sxStyle.StarterGapForMobile : sxStyle.StarterGap}>
-      <AppBar component="nav">
+      <AppBar component="nav" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
           <Typography variant="h6" sx={sxStyle.Typography} >
             <Link to={mainLogo.link} style={sxStyle.mainLogo}>
@@ -68,7 +68,7 @@ export default function MainNav(props) {
   );
 };
 
-const drawerWidth = 200;
+const drawerWidth = "-webkit-fill-available";
 const mainLogo = { label: 'KCURR', link: "/" }
 const navItems = [
   { label: 'Convertor', link: "/convertor" },
@@ -79,17 +79,12 @@ const navItems = [
 
 const PopupSideBar = ({ navItems, handleDrawerToggle }) => {
   return (
-    <Box onClick={handleDrawerToggle} sx={sxStyle.BoxPopupSideBar}>
-      <Typography variant="h6" sx={sxStyle.TypographyPopupSideBar}>
-        <img src={embbedLogo.link} alt={embbedLogo.alt} style={{...style.logo, filter: "none"}} />
-        {mainLogo.label}
-      </Typography>
-      <Divider />
-      <List>
+    <Box onClick={handleDrawerToggle}>
+      <List sx={sxStyle.ListPopupSideBar}>
         {navItems.map((item) => (
           <ListItem key={item.label} disablePadding>
             <ListItemButton sx={sxStyle.ListItemButtonPopupSideBar} href={item.link} >
-              <ListItemText primary={item.label}/>
+              <ListItemText primary={item.label} sx={{}}/>
             </ListItemButton>
           </ListItem>
         ))}
@@ -115,9 +110,10 @@ const sxStyle = {
     display: { xs: 'block', sm: 'none' },
     '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
   },
-  BoxPopupSideBar: { textAlign: 'center', color: '#1976d2' },
-  TypographyPopupSideBar: { my: 2, display: 'flex', justifyContent: 'center' },
-  ListItemButtonPopupSideBar: { textAlign: 'center' },
+  BoxPopupSideBar: { color: '#1976d2' },
+  ListPopupSideBar: { my: 8 },
+  ListItemPopupSideBar: {  },
+  ListItemButtonPopupSideBar: { textAlign: 'left', borderBottom: "1px solid #00000030", margin: "0px 20px" },
 }
 
 const style = {
