d77b30f3f2b8852f7ac366e7629d94a8f590debf
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-06-11 14:37:27 -0700

    handle routing bug with deployed environment

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -8,15 +8,18 @@ using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Set up the base URLs for development and production environments
 string devBaseURL = "http://localhost:3000";
 string prodBaseURL = "https://kcurr.onrender.com";
 string allowedOrigins = "";
 
+// Determind which baseURl should be used base on the environments
 if (builder.Environment.IsDevelopment())
     allowedOrigins = devBaseURL;
 else
     allowedOrigins = prodBaseURL;
 
+// Configures CORS to allow requests from the specified origins with the specified headers and methods.
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
@@ -29,8 +32,13 @@ builder.Services.AddCors(options =>
         });
 });
 
-builder.Services.AddControllers();
+// Add services for controllers with views
+builder.Services.AddControllersWithViews();
+
+// Add support for making HTTP requests
 builder.Services.AddHttpClient();
+
+// Configure dependency injection for various services
 builder.Services.AddSingleton<CurrService>();
 builder.Services.AddSingleton<FinancialNewsService>();
 builder.Services.AddSingleton<IExchangeRateApiClient, CurrencyBeaconApiClient>();
@@ -39,17 +47,31 @@ builder.Services.AddSingleton<ApiKeysProvider>();
  
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error"); // Use custom error page in production
+    app.UseHsts(); // Enable HSTS (HTTP Strict Transport Security) in production
+}
+
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("CORS Allowed Origins: {@CorsAllowedOrigins}", allowedOrigins);
 
-app.MapGet("/", () => "Hello From KCURR-Backend!!!");
+app.MapGet("/", () => "Hello From KCURR-Backend!!!"); // Define a simple endpoint for the root URL
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Redirect HTTP requests to HTTPS
 
-app.UseCors();
+app.UseCors(); // Enable CORS using the previously defined policy
 
-app.UseAuthorization();
+app.UseStaticFiles(); // Serve static files
 
-app.MapControllers();
+app.UseRouting(); // Enable routing
 
-app.Run();
+app.UseAuthorization(); // Enable authorization (middleware)
+
+app.MapControllers(); // Map controller routes
+
+// Serve the index.html file for all unknown routes to support client-side routing in a SPA
+app.MapFallbackToFile("index.html");
+
+app.Run(); // Run the application
