e97ae9a9ce73d7631d5033b99cfad604d35da1b7
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-06-16 22:31:23 -0700

    refactored getFlage's logic

--- frontend/src/util/getFlag.js
+++ frontend/src/util/getFlag.js
@@ -1,26 +1,26 @@
 export function getFlag(currCountry) {
-    if (currCountry == null || currCountry.substring(0, 1) === "X") {
+    if (isValidParam(currCountry))
+        return <img style={style.flagImg} src={getBaseUrl(currCountry)} alt=""/>
+    else
         return <div style={{...style.flagImg, ...style.divGetFlag}} >{currCountry}</div>
-    } else {
-        const url = getBaseUrl(currCountry);
-        if (UrlExists(url))
-            return <img style={style.flagImg} src={getBaseUrl(currCountry)} alt=""/>
-        else
-            return <div style={{...style.flagImg, ...style.divGetFlag}} >{currCountry}</div>
-    }
 };
+
+const isValidParam = (currCountry) => {
+    if (!(currCountry == null || currCountry.substring(0, 1) === "X") && isUrlExists(getBaseUrl(currCountry)))
+        return true;
+    else 
+        return false;
+}
 
 const getBaseUrl = (currCountry) => {
     return `https://purecatamphetamine.github.io/country-flag-icons/3x2/${currCountry.substring(0, 2).toUpperCase()}.svg`;
 }
-function UrlExists(url) {
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    if (http.status != 404)
-        return true;
-    else
-        return false;
+
+const isUrlExists = (url) => {
+    var req = new XMLHttpRequest();
+    req.open('HEAD', url, false);
+    req.send();
+    return req.status === 200;
 }
 
 const style = {
