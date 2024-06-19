1a3ea978ad18448e857c66a1e1f85fd17eb63365
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-06-18 23:19:35 -0700

    Optimized stable sorting logic's speed and performance

--- frontend/src/util/ExchangeRateTableDataUtil.js
+++ frontend/src/util/ExchangeRateTableDataUtil.js
@@ -5,15 +5,16 @@ export function getComparator(order, orderBy) {
 }
 
 export function stableSort(array, comparator) {
-    const stabilizedThis = array?.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
+    const stabilizedArray = Array.from({ length: array.length }, (v, i) => [array[i], i]);
+    stabilizedArray.sort((a, b) => {
         const order = comparator(a[0], b[0]);
         if (order !== 0) {
             return order;
         }
         return a[1] - b[1];
     });
-    return stabilizedThis.map((el) => el[0]);
+    
+    return stabilizedArray.map((el) => el[0]);
 }
 
 export function styleTableCell(currList, isDisplaySM) {
