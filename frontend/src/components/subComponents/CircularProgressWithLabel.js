a7d55b02b69c9421fabda5643a534b7894ad7ded
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-15 00:38:55 -0700

    solve the hidden grey bar on safari

--- frontend/src/components/subComponents/CircularProgressWithLabel.js
+++ frontend/src/components/subComponents/CircularProgressWithLabel.js
@@ -100,7 +100,7 @@ export default function CircularWithValueLabel(props) {
 const sxStyle = {
     Container: { display: 'flex', alignItems: 'center' },
     Box: { position: 'relative', display: 'inline-flex' },
-    CircularGrey: { color: (theme) => theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800] },
+    CircularGrey: { color: (theme) => theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800], '& svg': { position: 'inherit' } },
     CenterPos: {
         top: 0, left: 0, bottom: 0, right: 0, position: 'absolute', display: 'flex', alignItems: 'center',
         justifyContent: 'center'
