906c176014cf12518535ac365a9a19016e350ffb
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-08-30 00:53:15 -0700

    convert checkingMethods logic to ts

---
+++ frontend/src/util/checkingMethods.ts
@@ -0,0 +1,12 @@
+import { type CurrList } from "../lib/types";
+
+export function checkIfContainsOnlyNumbers(str: string): boolean {
+    return /^[0-9.]+$/.test(str);
+}
+
+export function checkIfExist(currLists: CurrList[], targetCurr: string): boolean {
+    for (let el of currLists) {
+        if (el.targetCurr === targetCurr) return true;
+    }
+    return false;
+}
