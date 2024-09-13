7a31f2ec497f382ee62a927d106a616193092a63
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-09-13 12:37:19 -0700

    update month range for label display 3m onward

--- backend/backend/Services/TimeseriesTransformer.cs
+++ backend/backend/Services/TimeseriesTransformer.cs
@@ -36,7 +36,7 @@ public class TimeseriesTransformer
                 string monthAbbr = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(monthNum); // get the abbr. of the month
                 string day = fullDate.Substring(fullDate.Length - 2); // get dd
                 dayRange[index] = day + " " + monthAbbr;
-                monthRange[index] = day + " " + monthAbbr + " " + year;
+                monthRange[index] = monthAbbr + " " + year;
                 double rate = timeSeries.Values[i]; // return rate
                 changingRates[index] = rate;
                 index--;
