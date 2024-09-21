a99dbfe9be3a19e85517761db59c8c626d1a7477
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-09-21 14:45:48 -0700

    debug log on currlist's data is not available

--- frontend/src/components/ExchangeRateTable/ExchangeRateTable.tsx
+++ frontend/src/components/ExchangeRateTable/ExchangeRateTable.tsx
@@ -368,6 +368,8 @@ export default function ExchangeRateTable(props: ExchangeRateTableProps) {
                                     const labelId = `enhanced-table-checkbox-${index}`;
                                     const isDefaultCurr = currList.targetCurr === currCodeArray[0];
 
+                                    console.log("check currList - ", currList.targetCurr, ": ", currList);
+
                                     return (
                                         <>
                                             <TableRow
