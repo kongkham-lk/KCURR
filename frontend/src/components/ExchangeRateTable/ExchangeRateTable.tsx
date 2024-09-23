a3dbddae68c4d9d5a137d450d0dddad0abe3cd84
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-09-22 21:46:19 -0700

    update dependency array
    do not update useEffect when currlists change on initial load

--- frontend/src/components/ExchangeRateTable/ExchangeRateTable.tsx
+++ frontend/src/components/ExchangeRateTable/ExchangeRateTable.tsx
@@ -116,24 +116,13 @@ export default function ExchangeRateTable(props: ExchangeRateTableProps) {
                 
                 // if cookie return null, that's mean this is first visited KCURR user, DO NOT update anything
                 if (newCurrLists !== null) {
-                    // let foundInvalidCurrList = false;
-                    // cross checking if each currList's change is NaN
-                    // for (let i = 1; i < currLists.length; i++) {
-                    //     if (currLists[i].change === "NaN") {
-                    //         currLists[i] = await createCurrLists(defaultCurrCode, currLists[i].targetCurr, defaultCurrExchangeRates, timeSeriesRangeLength, isChartFeatureEnable);
-                    //         foundInvalidCurrList = true;
-                    //     }
-                    // }
-                    // if (foundInvalidCurrList)
-                    //     await saveCurrListsToCookie(userId, newCurrLists);
-
                     setCurrLists(newCurrLists);
                 }
                 setIsInitialLoad(false);
             }
         }
         fetchUpdateOnInitialLoad();
-    }, [currCodeArray, currLists, isInitialLoad, userId])
+    }, [currCodeArray, isInitialLoad, userId])
 
     // Update new added currency code to visible row
     useEffect(() => {
