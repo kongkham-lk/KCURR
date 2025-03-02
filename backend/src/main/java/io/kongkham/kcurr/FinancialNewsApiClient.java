408718f1075d1affa69e5f1b0571e5278cf3f7ba
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-03-01 16:05:45 -0800

    remove java file

--- backend/src/main/java/io/kongkham/kcurr/FinancialNewsApiClient.java
+++
@@ -1,5 +0,0 @@
-package io.kongkham.kcurr;
-
-public interface FinancialNewsApiClient {
-    public FinancialNewsResponse[] getFinancialNews();
-}
--- backend/src/main/java/io/kongkham/kcurr/RapidApiApiResponseThumbnail.java
+++
@@ -1,8 +0,0 @@
-package io.kongkham.kcurr;
-
-import lombok.Data;
-
-@Data
-public class RapidApiApiResponseThumbnail {
-    private RapidApiApiResponseResolutions[] resolutions;
-}
